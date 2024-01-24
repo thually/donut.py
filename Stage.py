c5490cbc63f3cf1fb54175e4d41ac64133aa6819
Alejandro Salazar Mejía <alejosalazarmejia.2000@gmail.com>
2024-01-24 11:14:22 -0500

    Comments translated into English

--- Stage.py
+++ Stage.py
@@ -5,21 +5,20 @@ from matplotlib import pyplot as plt
 
 class Stage:
     """ 
-    Esta clase representa un escenario en el cual colocaremos un dona
-    y la proyectaremos sobre la pantalla, dada una iluminación y un observador.
-    Dado un punto de la dona X = (x, y, z), un observador F = (0, f, 0), y un plano y = d,  
-    donde f > d > R1+R2 > 0, queremos proyectar el punto X sobre el plano y = d con la perspectiva de F.
-    Dicha proyección será sobre una pantalla de num_pixels x num_pixels pixeles.
-    En cada pixel se renderizará el caracter correspondiente a la intensidad de luz que recibe
-    el punto más cernano de la dona proyectado sobre dicho pixel.
+    This class represents a stage in which we place a donut and project it onto the screen, given lighting and an observer.
+    Given a point on the donut X = (x, y, z), an observer F = (0, f, 0), and a plane y = d,
+    where f > d > R1+R2 > 0, we want to project point X onto the y = d plane with the perspective of F.
+    This projection will be onto a screen with num_pixels x num_pixels pixels.
+    Each pixel will render the character corresponding to the light intensity received by
+    the nearest point on the donut projected onto that pixel.
     """
 
-    def __init__(self, dona: Donut, light: np.array = np.array([0, -1, -1]), f: float = 10, d: float = 5, num_pixels: int = 30):
+    def __init__(self, donut: Donut, light: np.array = np.array([0, -1, -1]), f: float = 10, d: float = 5, num_pixels: int = 30):
         """
-        Inicializa el escenario.
+        Initializes the stage.
         """
-        assert f > d > dona.R1 + dona.R2 > 0, "f > d > R1 + R2 > 0"
-        self.dona = dona
+        assert f > d > donut.R1 + donut.R2 > 0, "f > d > R1 + R2 > 0"
+        self.donut = donut
         self.light = light
         self.f = f
         self.d = d
@@ -29,24 +28,24 @@ class Stage:
 
     def project(self):
         """
-        Dado un punto de la dona X = (x, y, z), un observador F = (0, f, 0), y un plano y = d,  
-        donde f > d > R1+R2 > 0, queremos proyectar el punto X sobre el plano y = d con la perspectiva de F.
-        Para esto, consideremos la recta que pasa por X y F. Esta recta intersecta al plano y = d en el punto
-        P = (x', d, z'). Queremos encontrar x' y z'.
-        La ecuación de la recta R que pasa por X y F es:
+        Given a point on the donut X = (x, y, z), an observer F = (0, f, 0), and a plane y = d,
+        where f > d > R1+R2 > 0, we want to project point X onto the plane y = d with the perspective of F.
+        For this, consider the line passing through X and F. This line intersects the plane y = d at point
+        P = (x', d, z'). We want to find x' and z'.
+        The equation of the line R passing through X and F is:
             R(t) = F + t * (X - F) = (0, f, 0) + t * (x, y - f, z)
-        Queremos encontrar t' tal que R intersecte al plano y = d. Es decir, queremos encontrar t' tal que:
+        We want to find t' such that R intersects the plane y = d. That is, we want to find t' such that:
             R(t') = (0, f, 0) + t' * (x, y - f, z) = (x', d, z')
-        De la segunda coordenada de la ecuación anterior, tenemos que:
+        From the second coordinate of the above equation, we have:
             f + t' * (y - f) = d
             t' = (d - f) / (y - f)
-        Sustituyendo t' en las primeras y terceras coordenadas, tenemos que:
+        Substituting t' into the first and third coordinates, we have:
             x' = t' * x = (d - f) * x / (y - f)
             z' = t' * z = (d - f) * z / (y - f)
         """
-        x, y, z = self.dona.points
-        oyo = np.reciprocal(y - self.f)  # Calculamos 1/(y - f)
-        # Proyectamos los puntos sobre el plano y = d
+        x, y, z = self.donut.points
+        oyo = np.reciprocal(y - self.f)  # Calculate 1/(y - f)
+        # Project points onto the y = d plane
         xp = (self.d - self.f) * x * oyo
         zp = (self.d - self.f) * z * oyo
 
@@ -54,17 +53,17 @@ class Stage:
 
     def luminance(self):
         """
-        Calcula la luminancia de cada punto de la dona como el negativo 
-        del producto punto entre el vector normal y el vector de luz.
+        Calculates the luminance of each point on the donut as the negative
+        of the dot product between the normal vector and the light vector.
         """
-        return - self.light @ self.dona.normals
+        return - self.light @ self.donut.normals
 
     def animation(self, A: float, B: float):
         """
-        Esta función crea un gif de la dona proyectada en 2D rotando.
+        This function creates a gif of the projected donut in 2D rotation.
         """
         from matplotlib.animation import FuncAnimation
-        # Graficamos la dona proyectada
+        # Plot the projected donut
         xp, zp, _ = self.project()
         L = self.luminance()
         fig, ax = plt.subplots()
@@ -73,15 +72,15 @@ class Stage:
         ax.set_ylabel('Z')
         ax.set_aspect('equal')
         fig.savefig('projected_dona.png')
-        I = (self.dona.R2 + self.dona.R1) * (self.f - self.d) / self.f
+        I = (self.donut.R2 + self.donut.R1) * (self.f - self.d) / self.f
         ax.set(xlim=[-1.1*I, 1.1*I], ylim=[-1.1*I, 1.1*I])
 
         def animate(frame, *fargs):
-            dona = fargs[0]
+            donut = fargs[0]
             A = fargs[1]
             B = fargs[2]
-            dona.rotate_x(A)
-            dona.rotate_y(B)
+            donut.rotate_x(A)
+            donut.rotate_y(B)
             xp, zp, _ = self.project()
             # L = self.luminance()
             sc.set_offsets(np.c_[xp, zp])
@@ -89,98 +88,98 @@ class Stage:
             return sc,
 
         anim = FuncAnimation(fig, animate, frames=90, fargs=(
-            self.dona, A, B), interval=50, blit=True)
+            self.donut, A, B), interval=50, blit=True)
         print("Creating gif...")
         anim.save('projected_dona.gif', dpi=100)
         plt.close()
 
     def render_frame(self, A: float, B: float) -> np.ndarray:
         """
-        Renderiza un frame de la dona rotada A radianes en el eje x y B radianes en el eje y.
+        Renders a frame of the rotated donut A radians on the x-axis and B radians on the y-axis.
         """
-        # Reiniciamos la pantalla
+        # Reset the screen
         self.screen = np.full((self.num_pixels, self.num_pixels), " ")
-        # Rotamos la dona
-        self.dona.rotate_x(A)
-        self.dona.rotate_y(B)
-        # Obtenemos los puntos rotados
+        # Rotate the donut
+        self.donut.rotate_x(A)
+        self.donut.rotate_y(B)
+        # Get the rotated points
         xp, zp, oyo = self.project()
 
-        """ Calculamos la intensidad de luz que recibe cada punto
-        como el negativo del producto punto entre el vector normal y el vector de luz.
-        Entre más cercano a 1, más iluminado está el punto.
-        Con esto, podemos asignar un caracter a cada punto. Si L < 0, el punto
-        no está iluminado, por lo que se asigna un '.' .
-        Si L > 0, el punto está iluminado, por lo que se asigna un caracter
-        de la lista illumination según la intensidad de luz.
+        """ Calculate the light intensity received by each point
+        as the negative of the dot product between the normal vector and the light vector.
+        Closer to 1 means more illuminated the point is.
+        With this, we can assign a character to each point. If L < 0, the point
+        is not illuminated, so a '.' is assigned.
+        If L > 0, the point is illuminated, so a character
+        from the illumination list is assigned according to the light intensity.
         """
         L = self.luminance()
-        Lrange = (0, 0.9*L.max())  # Rango de valores de L.
-        # Multiplicamos por 0.9 para que la dona esté un poco más iluminada.
-        # Mapeamos los valores de L al rango de indices de la lista illumination
+        Lrange = (0, 0.9*L.max())  # Range of L values.
+        # Multiply by 0.9 so that the donut is a bit more illuminated.
+        # Map the values of L to the range of indices of the illumination list
         char_index = np.interp(
             L, Lrange, (0, len(self.illumination)-1)).astype(int)
         chars = self.illumination[char_index]
 
         """ 
-        Ahora, queremos proyectar los puntos sobre los correspondientes pixeles de la pantalla.
-        Para ello, primero calculamos el intervalo de valores de x y z que abarca la dona proyectada.
-        Para *estimar* este rango, es suficiente calcular la proyección del punto más alto de la dona.
-        Dicho punto es (0, 0, R2 + R1).
-        Usando semejanza de triángulos. La incognita es L:
+        Now, we want to project the points onto the corresponding pixels of the screen.
+        To do this, first calculate the interval of x and z values covered by the projected donut.
+        To *estimate* this range, it is sufficient to calculate the projection of the highest point on the donut.
+        That point is (0, 0, R2 + R1).
+        Using triangle similarity. The unknown is I:
             I / (R2 + R1) = (f - d) / f
             I = (R2 + R1) * (f - d) / f
-        Así tenemos que los valores de xp y zp están en un intervalo un poco más amplio que [-I, I], 
-        digamos [-1.1*I, 1.1*I].
+        So we have that the values of xp and zp are in a slightly wider interval than [-I, I],
+        say [-1.1*I, 1.1*I].
         """
-        I = (self.dona.R2 + self.dona.R1) * (self.f - self.d) / self.f
+        I = (self.donut.R2 + self.donut.R1) * (self.f - self.d) / self.f
 
         """
-        Siguiente, queremos mapear los valores de xp y zp al intervalo de enteros [0, num_pixels - 1].
-        Es decir, queremos mapear el intervalo [-1.1*I, 1.1*I] al intervalo de enteros [0, num_pixels - 1].
+        Next, we want to map the values of xp and zp to the range of integers [0, num_pixels - 1].
+        That is, we want to map the interval [-1.1*I, 1.1*I] to the interval of integers [0, num_pixels - 1].
         """
         xp = np.interp(xp, (-1.1*I, 1.1*I),
                        (0, self.num_pixels - 1)).astype(int)
         zp = np.interp(-zp, (-1.1*I, 1.1*I),
                        (0, self.num_pixels - 1)).astype(int)
-        # -zp porque las filas de la pantalla van de 0 a num_pixels - 1 de arriba hacia abajo
+        # -zp because the rows of the screen go from 0 to num_pixels - 1 from top to bottom
 
         """
-        Por último, queremos asignar un caracter a cada pixel. El caracter que le corresponde
-        a cada pixel es el caracter del punto más cercano a la pantalla. Esta información la podemos
-        obtener de la variable 'oyo' (0y0).
-        Sean X = (x, y, z) y Y = (x', y', z') dos puntos de la dona. Notemos que y - f < 0 y y' - f < 0
-        pues f > R1 + R2 > 0. Entonces, y < y' si y solo si:
+        Finally, we want to assign a character to each pixel. The character that corresponds
+        to each pixel is the character of the point nearest to the screen. This information can be
+        obtained from the variable 'oyo' (0y0).
+        Let X = (x, y, z) and Y = (x', y', z') be two points on the donut. Note that y - f < 0 and y' - f < 0
+        since f > R1 + R2 > 0. So, y < y' if and only if:
             <=>          y < y'
             <=>      y - f < y' - f
             <=> 1/(y' - f) < 1/(y - f)
-        Esto quiere decir que si ordenamos nuestros puntos (xp, zp) de forma descendente según el valor
-        de oyo, entonces el primer punto (xp[0], zp[0]) es el punto más lejano a la pantalla, y el último
-        punto (xp[-1], zp[-1]) es el punto más cercano a la pantalla.
+        This means that if we order our points (xp, zp) in descending order according to the value
+        of oyo, then the first point (xp[0], zp[0]) is the point furthest from the screen, and the last
+        point (xp[-1], zp[-1]) is the point closest to the screen.
         """
-        # Ordenamos los puntos según el valor de oyo
+        # Sort the points according to the value of oyo
         order = np.argsort(oyo)[::-1]
         xp = xp[order]
         zp = zp[order]
         chars = chars[order]
-        # Asignamos los caracteres a los pixeles
-        # zp, xp pues las filas de la pantalla representan el eje 'y' y las columnas el eje 'x'
+        # Assign characters to pixels
+        # zp, xp because the rows of the screen represent the 'y' axis and the columns the 'x' axis
         self.screen[zp, xp] = chars
         """
-        Por qué la línea anterior funciona? Note que es posible que dos puntos x1 = (x1, y1, z1) y x2 = (x2, y2, z2) se proyecten en el mismo pixel.
-        Supongamos que (xp1, zp1) son las coordenadas del pixel al que se proyecta x1, (xp2, zp2) son las coordenadas del pixel al que se proyecta x2,
-        el caracter asignado a x1 es c1 y el caracter de x2 es c2.
-        Se tiene entonces que (xp1, zp1) == (xp2, zp2). Ahora, asumamos sin pérdida de generalidad que (xp1, zp1)
-        aparece antes que (xp2, zp2) en 'order'.
-        Como (xp1, zp1) aparece antes que (xp2, zp2) en 'order', (xp1, zp1) está más lejos de la pantalla que (xp2, zp2).
-        Al hacer self.screen[zp, xp] = chars, primero se asigna el caracter c1 al pixel (xp1, zp1) porque aparece primero en 'order' 
-        y luego se asigna el caracter c2 al pixel (xp2, zp2).
-        Pero (xp1, zp1) == (xp2, zp2) así que dicho pixel se asigna dos veces, primero con el caracter c1 y luego con el caracter c2.
-        Así que el caracter que se muestra en el pixel (xp2, zp2) es correctamente el caracter c2 ya que (xp2, zp2) está más cerca de la pantalla.
+        Why does the above line work? Note that it is possible that two points x1 = (x1, y1, z1) and x2 = (x2, y2, z2) are projected onto the same pixel.
+        Suppose (xp1, zp1) are the coordinates of the pixel to which x1 is projected, (xp2, zp2) are the coordinates of the pixel to which x2 is projected,
+        the character assigned to x1 is c1, and the character of x2 is c2.
+        Then (xp1, zp1) == (xp2, zp2). Now, assuming without loss of generality that (xp1, zp1)
+        appears before (xp2, zp2) in variable 'order'.
+        Since (xp1, zp1) appears before (xp2, zp2) in 'order', (xp1, zp1) is further from the screen than (xp2, zp2).
+        By doing self.screen[zp, xp] = chars, the character c1 is first assigned to the pixel (xp1, zp1) because it appears first in 'order'
+        and then the character c2 is assigned to the pixel (xp2, zp2).
+        But (xp1, zp1) == (xp2, zp2) so that pixel is assigned twice, first with the character c1 and then with the character c2.
+        So the character shown in pixel (xp2, zp2) is correctly the character c2 since (xp2, zp2) is closer to the screen.
         """
 
     def pprint(self) -> None:
         """ 
-        Imprime los pixeles guardados en 'screen'.
+        Prints the pixels stored in 'screen'.
         """
         print(*[" ".join(row) for row in self.screen], sep="\n", flush=True)
